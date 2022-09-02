58e9b4f50d20c32b5d27f081c7812a4bcc55a9af
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2022-09-02 21:38:45 +0200

    Add precedes and extends listchars

--- lua/bryanboateng/set.lua
+++ lua/bryanboateng/set.lua
@@ -7,7 +7,15 @@ vim.opt.smartindent = true
 
 vim.opt.hlsearch = false
 
-vim.opt.listchars="eol:\\u00AC,tab:\\u00BB\\u0020,space:\\u00B7,trail:\\u00D7,nbsp:\\u00B0"
+vim.opt.listchars= {
+	eol = '\\u00AC',
+	tab = '\\u00BB\\u0020',
+	space = '\\u00B7',
+	precedes = '\\u27E8',
+	extends = '\\u27E9',
+	trail = '\\u00D7',
+	nbsp = '\\u00B0'
+}
 vim.opt.list = true
 
 vim.opt.scrolloff=8
