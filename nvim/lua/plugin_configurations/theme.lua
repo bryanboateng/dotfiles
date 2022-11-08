ecdf513ecb012dc21cb677c8863be757f805165b
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2022-11-08 21:53:39 +0100

    Neovim: Set background color
    Fixes Linenumber color bug

--- nvim/lua/plugin_configurations/theme.lua
+++ nvim/lua/plugin_configurations/theme.lua
@@ -4,5 +4,7 @@ require("github-theme").setup({
 	theme_style = "dark_default",
 	hide_end_of_buffer = false,
 	hide_inactive_statusline = false,
-	transparent = true
+	colors = {
+		bg = "#000000",
+	}
 })
