d9e5a465e82982269cd7539ffc0132ca7f33685a
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2022-12-02 15:04:15 +0100

    Change tab with to 3 columns

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -1,8 +1,8 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 3
+vim.opt.shiftwidth = 3
 vim.opt.smartindent = true
 
 vim.opt.hlsearch = false
